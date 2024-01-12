7fe1d6cce38e55bc178955afb248ceba2b6a7c66
José Ibanez <appibanez@gmail.com>
2024-01-12 20:03:11 +0100

    cleaning project

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,8 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1>Hola mundo</h1>
+    <h5>Homeview</h5>
   </main>
 </template>
