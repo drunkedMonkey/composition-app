d1c4f46a47461a278dc5316be0edc6a6979e2e84
José Ibanez <appibanez@gmail.com>
2024-01-19 22:25:36 +0100

    viuQuery added

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,24 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { VueQueryPlugin } from '@tanstack/vue-query'
 
-const app = createApp(App)
+import './assets/main.css'
 
+const app = createApp(App);
+
+VueQueryPlugin.install(app,{
+    queryClientConfig: {
+        defaultOptions: {
+            queries: {
+                gcTime: 1000 * 120,
+                refetchOnReconnect: 'always',
+            },
+        }
+    },
+})
+
+// app.use(VueQueryPlugin)
 app.use(router)
 
 app.mount('#app')
